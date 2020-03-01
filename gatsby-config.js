3fb86a4b54d1f05850a308cd04dcbefa59f30c03
Francisco Costa <francisco@fcosta.pt>
2020-03-01 19:50:45 +0000

    Created footer and changed metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dev Fran`,
+    description: `Dev Fran - This is my personal website.`,
+    author: `Francsico Cosra`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
