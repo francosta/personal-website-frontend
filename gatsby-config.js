6033960d76d892ba82fa3ff2d9b1dd30dcbb6ccc
Francisco Costa <francisco@fcosta.pt>
2020-03-01 18:01:22 +0000

    Contentful data can be queried in GRAPHQL playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
