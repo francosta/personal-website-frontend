54a2bd6fe4e41c25155cd024573ecea669c43bda
Francisco Costa <francisco@fcosta.pt>
2020-03-02 19:57:00 +0000

    Created navbar components and  imported into header.

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <Link>Home</Link>
+        <Link>About</Link>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
