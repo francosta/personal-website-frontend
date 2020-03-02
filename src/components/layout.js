c06b8721de251b778c30f455e1b7c81089ce5148
Francisco Costa <francisco@fcosta.pt>
2020-03-02 20:01:48 +0000

    Created navbar components and  imported into header.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.scss"
-import Footer from "./footer"
+import Footer from "./Footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
