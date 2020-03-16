ea4c2d00f7ed73deab8c35919697df1687d85011
francosta <francisco@fcosta.pt>
2020-03-16 15:53:33 +0000

    Did card, env variables not working.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react"
+const path = require("path")
+const env = require("dotenv").config({ path: `${path.resolve()}/.env` })
+
+const Card = ({ post }) => {
+  return (
+    <div>
+      {/* <img
+        src={`${process.env.API_URL}${post.node.hero.image.url}`}
+        alt={post.node.hero.image.altText}
+      /> */}
+      <p>{`${process.env.API_URL}${post.node.hero.image.url}`}</p>
+    </div>
+  )
+}
+
+export default Card
