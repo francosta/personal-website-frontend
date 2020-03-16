2a2dd6aa9c67757b120e316570bdb80d0f6068f6
Francisco Costa <francisco@fcosta.pt>
2020-03-16 18:33:56 +0000

    Added styling to cards and container

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,12 +5,14 @@ import cardStyles from "./card.module.scss"
 const Card = ({ post }) => {
   return (
     <div className={cardStyles.container}>
-      <img
-        className={cardStyles.image}
-        src={`${process.env.API_URL}${post.node.hero.image.url}`}
-        alt={post.node.hero.image.altText}
-      />
-      <h4 className={cardStyles.text}>{post.node.title}</h4>
+      <div className={cardStyles.card}>
+        <img
+          className={cardStyles.image}
+          src={`${process.env.API_URL}${post.node.hero.image.url}`}
+          alt={post.node.hero.image.altText}
+        />
+        <h4 className={cardStyles.text}>{post.node.title}</h4>
+      </div>
     </div>
   )
 }
