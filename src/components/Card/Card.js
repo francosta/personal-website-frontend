d32494cf5023239c50caa25b65aac19159b27cdd
Francisco Costa <francisco@fcosta.pt>
2020-03-18 12:30:25 +0000

    Made blog grid responsive

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,24 +3,31 @@ import React from "react"
 import cardStyles from "./card.module.scss"
 
 const Card = ({ index, post }) => {
-  if (index === 0) {
+  if (index % 2 === 0 || index === 0) {
     return (
-      <div className={`${cardStyles.card2} ${cardStyles.card}`}>
+      <div className={`${cardStyles.card} ${cardStyles.card1} `}>
         <img
           className={cardStyles.image}
           src={`${process.env.API_URL}${post.node.hero.image.url}`}
           alt={post.node.hero.image.altText}
         />
+        <h1>{post.node.title}</h1>
+        <h6>{post.node.description}</h6>
       </div>
     )
   } else {
     return (
-      <div className={`${cardStyles.card1} ${cardStyles.card}`}>
+      <div className={`${cardStyles.card} ${cardStyles.card2} `}>
         <img
           className={cardStyles.image}
           src={`${process.env.API_URL}${post.node.hero.image.url}`}
           alt={post.node.hero.image.altText}
         />
+        <h1>{post.node.title}</h1>
+        <h6>{post.node.description}</h6>
+        <div className={cardStyles.details}>
+          
+        </div>
       </div>
     )
   }
