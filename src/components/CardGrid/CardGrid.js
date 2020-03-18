a71f83c54cf6a9e1df492bcc1d0e7bcccdea8b6c
Francisco Costa <francisco@fcosta.pt>
2020-03-18 10:42:10 +0000

    Changes styling on index and layout and added global styles.

--- src/components/CardGrid/CardGrid.js
+++ src/components/CardGrid/CardGrid.js
@@ -7,7 +7,7 @@ const CardGrid = ({ posts }) => {
   return (
     <div className={cardGridStyles.cardGrid}>
       {posts.map((post, index) => {
-        return <Card key={index} post={post} />
+        return <Card index={index} key={index} post={post} />
       })}
     </div>
   )
