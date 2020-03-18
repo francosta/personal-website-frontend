1555c0dc1374b7f2c2216c42b5d48b2bc79656b9
Francisco Costa <francisco@fcosta.pt>
2020-03-18 10:42:10 +0000

    Changes styling on index and layout and added global styles.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,20 +2,28 @@ import React from "react"
 
 import cardStyles from "./card.module.scss"
 
-const Card = ({ key, post }) => {
-  return (
-    // <div className={cardStyles.container}>
-    //   <div className={cardStyles.card}>
-    <div className={cardStyles.card2}>
-      <img
-        className={cardStyles.image}
-        src={`${process.env.API_URL}${post.node.hero.image.url}`}
-        alt={post.node.hero.image.altText}
-      />
-      {/* <h4 className={cardStyles.text}>{post.node.title}</h4> */}
-    </div>
-    // </div>
-  )
+const Card = ({ index, post }) => {
+  if (index === 0) {
+    return (
+      <div className={`${cardStyles.card2} ${cardStyles.card}`}>
+        <img
+          className={cardStyles.image}
+          src={`${process.env.API_URL}${post.node.hero.image.url}`}
+          alt={post.node.hero.image.altText}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <div className={`${cardStyles.card1} ${cardStyles.card}`}>
+        <img
+          className={cardStyles.image}
+          src={`${process.env.API_URL}${post.node.hero.image.url}`}
+          alt={post.node.hero.image.altText}
+        />
+      </div>
+    )
+  }
 }
 
 export default Card
