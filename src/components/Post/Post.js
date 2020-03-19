e0f9099abcf7224ab4818a2df308b7198489a768
francosta <francisco@fcosta.pt>
2020-03-19 16:57:19 +0000

    Added some styling to post template

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,15 +1,19 @@
 import React from "react"
 
+import ReactMarkdown from "react-markdown"
+
 import postStyles from "./post.module.scss"
 
 const Post = ({ post }) => {
   return (
-    <div className={postStyles.post}>
+    <div className={postStyles.container}>
       <img
+        className={postStyles.hero}
         src={`${process.env.API_URL}${post.hero.image.url}`}
         alt={post.hero.image.altText}
       />
-      <h1>{post.title}</h1>
+      <h1 className={postStyles.title}>{post.title}</h1>
+      <ReactMarkdown className={postStyles.body} source={post.body} />
     </div>
   )
 }
