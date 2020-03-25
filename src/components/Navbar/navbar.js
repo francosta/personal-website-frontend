72af5a0d42020b1e5723eba8b4e5352d9c8b4f1c
Francisco Costa <francisco@fcosta.pt>
2020-03-25 23:06:49 +0000

    Navbar still not done

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,8 +1,15 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import navbarStyles from "./navbar.module.scss"
 
 const Navbar = () => {
+  const navbarSlide = () => {
+    const navLinks = document.querySelector(
+      ".navbar-module--navbar-links--21od5"
+    )
+    navLinks.classList.toggle("nav-active")
+  }
+
   return (
     <nav className={`${navbarStyles.navbar}`}>
       <div className={navbarStyles.logo}>
@@ -10,7 +17,7 @@ const Navbar = () => {
           <h1 id={navbarStyles.logo}>Francisco Costa</h1>
         </Link>
       </div>
-      <ul className={navbarStyles.navbarLinks}>
+      <ul className={`${navbarStyles.navbarLinks} ${navbarStyles.navActive}`}>
         <li>
           <Link to="/about">About</Link>
         </li>
@@ -24,7 +31,7 @@ const Navbar = () => {
           <Link to="/">Contact</Link>
         </li>
       </ul>
-      <div className={navbarStyles.burger}>
+      <div className={navbarStyles.burger} onClick={navbarSlide}>
         <div className={navbarStyles.line1}></div>
         <div className={navbarStyles.line1}></div>
         <div className={navbarStyles.line1}></div>
