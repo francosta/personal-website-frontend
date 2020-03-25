d31b0d838a6359d6b2ecfdbada5bfe768f9feac9
francosta <francisco@fcosta.pt>
2020-03-25 18:11:55 +0000

    Deleted changes to navbar.

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,37 +3,13 @@ import { Link } from "gatsby"
 import navbarStyles from "./navbar.module.scss"
 
 const Navbar = () => {
-  // document.body.classList.toggle("js-enabled")
-  let hamburger = document.getElementById(navbarStyles.hamburger)
-  hamburger.setAttribute("aria-expanded", "false")
-
-  hamburger.onclick = function() {
-    if (this.getAttribute("aria-expanded") == "false") {
-      this.setAttribute("aria-expanded", "true")
-    } else {
-      this.setAttribute("aria-expanded", "false")
-    }
-  }
-
   return (
-    <nav className={`${navbarStyles.navbar} ${navbarStyles.jsEnabled}`}>
+    <nav className={`${navbarStyles.navbar}`}>
       <div className={navbarStyles.logo}>
         <Link to="/">
           <h1 id={navbarStyles.logo}>Francisco Costa</h1>
         </Link>
       </div>
-      <button id={navbarStyles.hamburger} aria-expanded="true">
-        <span>menu</span>
-        <span id={navbarStyles.expanded}>expanded</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
-        </svg>
-      </button>
       <ul className={navbarStyles.navbarLinks}>
         <li>
           <Link to="/about">About</Link>
