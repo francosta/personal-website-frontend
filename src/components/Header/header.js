3a5a93dd37e8b09e8589c6487d99d502e5af9b4d
Francisco Costa <francisco@fcosta.pt>
2020-04-04 11:54:01 +0100

    Added favicon and site title to Header component, via react helmet

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types"
 import React from "react"
 import Navbar from "../Navbar/navbar"
+import Helmet from "react-helmet"
+import favicon from "../../images/code-icon.svg"
 
 const Header = ({ siteTitle }) => (
   <header>
+    <Helmet>
+      <link rel="icon" href={favicon} />
+      <title>{siteTitle}</title>
+    </Helmet>
     <Navbar />
   </header>
 )
