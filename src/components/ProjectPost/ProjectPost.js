989082ce1a265307e5c69c7c3434689cf704eb37
Francisco Costa <francisco@fcosta.pt>
2020-04-06 18:30:36 +0100

    Left side of details box already done.

--- src/components/ProjectPost/ProjectPost.js
+++ src/components/ProjectPost/ProjectPost.js
@@ -22,21 +22,23 @@ const Post = ({ post }) => {
       </section>
       <section className={postStyles.summaryBox}>
         <div className={postStyles.specs}>
-          <div className={postStyles.date}>
-            <div>
+          <div className={postStyles.dateBox}>
+            <div className={postStyles.date}>
               <h2>Date of Project</h2>
               <span>{post.publishedDate}</span>
             </div>
             <img src={dateIcon}></img>
           </div>
-          <div className={postStyles.stack}>
-            <h2>Stack</h2>
+          <div className={postStyles.stackBox}>
+            <div className={postStyles.stack}>
+              <h2>Stack</h2>
+              <ul>
+                {post.stack.technology.map(technology => (
+                  <li>{technology.name}</li>
+                ))}
+              </ul>
+            </div>
             <img src={stackIcon}></img>
-            <ul>
-              {post.stack.technology.map(technology => (
-                <li>{technology.name}</li>
-              ))}
-            </ul>
           </div>
         </div>
         <div className={postStyles.details}>
