8adf200c945f3e510f140acb2f459c5530706564
Francisco <francisco@fcosta.pt>
2020-04-13 16:35:59 +0100

    Fix/navbar margins (#43)
    * Cleaned code according to new linting rules.
    
    * Fixed issue with navbar.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,8 +1,8 @@
-import PropTypes from "prop-types"
-import React from "react"
-import Navbar from "../Navbar/navbar"
-import { Helmet } from "react-helmet"
-import favicon from "../../images/code-icon.svg"
+import PropTypes from 'prop-types';
+import React from 'react';
+import Navbar from '../Navbar/navbar';
+import { Helmet } from 'react-helmet';
+import favicon from '../../images/code-icon.svg';
 
 const Header = ({ siteTitle }) => (
   <header>
@@ -10,16 +10,15 @@ const Header = ({ siteTitle }) => (
       <link rel="icon" href={favicon} />
       <title>{siteTitle}</title>
     </Helmet>
-    <Navbar />
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
