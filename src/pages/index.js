34a0f95a6bf637cf89e21b90baf6fe6b041fdd9b
Francisco Costa <francisco@fcosta.pt>
2020-04-15 15:18:24 +0100

    Added padding to posts section on homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Layout from "../components/Layout/layout"
-import SEO from "../components/seo"
-import styles from "../styles/index.scss"
-import indexStyles from "./index.module.scss"
-import CardGrid from "../components/CardGrid/CardGrid"
+import Layout from '../components/Layout/layout';
+import SEO from '../components/seo';
+import styles from '../styles/index.scss';
+import indexStyles from './index.module.scss';
+import CardGrid from '../components/CardGrid/CardGrid';
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -56,13 +56,13 @@ const IndexPage = () => {
         landingText
       }
     }
-  `)
+  `);
 
-  const posts = data.allStrapiBlogPost.nodes.concat(data.allStrapiProject.nodes)
+  const posts = data.allStrapiBlogPost.nodes.concat(
+    data.allStrapiProject.nodes
+  );
 
-  console.log(posts)
-
-  const landingText = data.strapiLandingPage.landingText
+  const { landingText } = data.strapiLandingPage;
 
   return (
     <Layout>
@@ -80,7 +80,7 @@ const IndexPage = () => {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g clip-path="url(#clip0)">
+              <g clipPath="url(#clip0)">
                 <path
                   d="M64.2298 68.5457L118.209 28.343C120.572 26.579 120.582 23.7076 118.231 21.9226C115.88 20.142 112.051 20.1287 109.688 21.8928L59.9921 58.9066L10.5555 21.5516C8.20453 19.7711 4.37584 19.7578 2.0065 21.5218C-0.356876 23.2859 -0.366902 26.1618 1.98411 27.9423L55.6813 68.5204C58.008 70.274 61.8969 70.2875 64.2298 68.5457Z"
                   fill="black"
@@ -102,11 +102,11 @@ const IndexPage = () => {
           </a>
         </div>
       </section>
-      <section id="posts">
+      <section id="posts" className={indexStyles.posts}>
         <CardGrid posts={posts} />
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
