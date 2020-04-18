190e883b86c10f56eb94ef77df4a670051475eee
Francisco Costa <francisco@fcosta.pt>
2020-04-18 12:23:42 +0100

    Added alt attributes to logon on footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,21 @@
-import React from "react"
-import footerStyles from "./footer.module.scss"
-import linkedinLogo from "../../images/linkedin.png"
-import githubLogo from "../../images/github.png"
+import React from 'react';
+import footerStyles from './footer.module.scss';
+import linkedinLogo from '../../images/linkedin.png';
+import githubLogo from '../../images/github.png';
 
 const Footer = () => {
   return (
     <footer>
       <a href="https://github.com/francosta/" target="blank">
-        <img src={githubLogo}></img>
+        <img src={githubLogo} alt="Gitub logo" />
         <h1>Github</h1>
       </a>
       <a href="https://www.linkedin.com/in/francosta/" target="blank">
-        <img src={linkedinLogo}></img>
+        <img src={linkedinLogo} alt="Linkedin logo" />
         <h1>Linkedin</h1>
       </a>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
