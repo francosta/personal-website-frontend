872be0371aac1633e7a42320c3f0736c29671737
francosta <francisco@fcosta.pt>
2020-04-19 14:06:35 +0100

    Added style to images.

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -15,7 +15,7 @@ const Post = ({ post }) => {
       />
       <h1 className={postStyles.title}>{post.title}</h1>
       <h2 className={postStyles.description}>{post.description}</h2>
-      <div className={postStyles.details}>
+      {/* <div className={postStyles.details}>
         <Link to="/about">
           <img
             className={postStyles.avatar}
@@ -34,7 +34,7 @@ const Post = ({ post }) => {
             <span>{Math.round(post.readingTime.minutes)}min read</span>
           </div>
         </div>
-      </div>
+      </div> */}
       <ReactMarkdown className={postStyles.body}>{post.body}</ReactMarkdown>
     </div>
   );
