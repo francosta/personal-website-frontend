7d406ac373bc5cadb4891bec85f3a7565fb68c19
Francisco <francisco@fcosta.pt>
2020-04-20 14:43:09 +0100

    Feature/card tags (#62)
    * Added tags
    
    * Removed tags from blog and project pages.
    
    * Added ESLint accessibility corrections.

--- src/components/CardGrid/CardGrid.js
+++ src/components/CardGrid/CardGrid.js
@@ -1,16 +1,16 @@
-import React from "react"
-import cardGridStyles from "./cardGrid.module.scss"
+import React from 'react';
+import cardGridStyles from './cardGrid.module.scss';
 
-import Card from "../Card/Card"
+import Card from '../Card/Card';
 
-const CardGrid = ({ posts }) => {
+const CardGrid = ({ posts, path }) => {
   return (
     <div className={cardGridStyles.cardGrid}>
       {posts.map((post, index) => {
-        return <Card index={index} key={index} post={post} />
+        return <Card path={path} index={index} key={index} post={post} />;
       })}
     </div>
-  )
-}
+  );
+};
 
-export default CardGrid
+export default CardGrid;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Navbar from '../Navbar/navbar';
 import { Helmet } from 'react-helmet';
 import favicon from '../../images/code-icon.svg';
 
