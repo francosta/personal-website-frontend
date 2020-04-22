d961b96e404a00ac1bcf213cdf132dcb974cf76e
Francisco <francisco@fcosta.pt>
2020-04-22 23:51:46 +0100

    Feature/contact page (#66)
    * Created barebones contact page.
    
    * Continued contact page. Still some styling to do.
    
    * Added animation to contact and index pages.
    
    * More styling.
    
    * Added some styling to contact page. Still need to finish responsiveness.
    
    * Responsiveness working for Pixel 2XL. Still need to make adjustments for other devices but almost there.

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -57,7 +57,7 @@ const Navbar = ({ path }) => {
                 : null
             }
           >
-            <Link to="/">Contact</Link>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
         <div
