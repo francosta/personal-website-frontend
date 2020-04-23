a348b5d75b00a570984ce1d93bdbb712ace7650c
Francisco Costa <francisco@fcosta.pt>
2020-04-23 20:30:58 +0100

    Started form handling

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -3,6 +3,24 @@ import Layout from '../../components/Layout/layout';
 import contactStyles from './contact.module.scss';
 
 const Contact = ({ path }) => {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const formData = {
+  //     name: e.target.name.value,
+  //     email: e.target.email.value,
+  //     message: e.target.message.value,
+  //   };
+
+  //   const url = `https://getform.io/f/${process.env.EMAIL_API}`;
+
+  //   console.log(url);
+
+  //   const options = {
+  //     method: 'POST',
+  //     data: formData,
+  //   };
+  // };
+
   return (
     <Layout path={path}>
       <div className={contactStyles.container}>
@@ -10,7 +28,11 @@ const Contact = ({ path }) => {
           Leave me a message and I’ll get back to you as soon as possible.
           Alternatively, you can also reach me at francisco@fcosta.pt
         </p>
-        <form>
+        <form
+          // onSubmit={handleSubmit}
+          action="https://getform.io/f/cca89e21-6ebd-420d-b25a-0f65efbde0aa"
+          method="POST"
+        >
           <div className={contactStyles.nameForm}>
             <label htmlFor="name">Name</label>
             <input type="text" id="name" name="name" />
