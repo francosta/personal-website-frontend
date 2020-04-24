e71228f68f16b349b3741a5538539de5ed1225b4
Francisco Costa <francisco@fcosta.pt>
2020-04-24 18:07:59 +0100

    Added form handling and message. Need to add form validation and styling of success message to center on mobile. Need to add error message.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,53 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/Layout/layout';
 import contactStyles from './contact.module.scss';
 
 const Contact = ({ path }) => {
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const formData = {
-  //     name: e.target.name.value,
-  //     email: e.target.email.value,
-  //     message: e.target.message.value,
-  //   };
+  const [messageSent, setMessageSent] = useState(false);
+  const [error, setError] = useState('');
 
-  //   const url = `https://getform.io/f/${process.env.EMAIL_API}`;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      message: e.target.message.value,
+    };
 
-  //   console.log(url);
+    const url = `https://getform.io/f/${process.env.EMAIL_API}`;
 
-  //   const options = {
-  //     method: 'POST',
-  //     data: formData,
-  //   };
-  // };
+    const options = {
+      method: 'POST',
+      data: formData,
+    };
+
+    try {
+      const resp = await fetch(url, options);
+      await setMessageSent(true);
+    } catch (e) {
+      setError("Something's gone wrong! I'm sorry. Please try again later :)");
+    }
+  };
 
   return (
     <Layout path={path}>
-      <div className={contactStyles.container}>
-        <p className={contactStyles.message}>
-          Leave me a message and I’ll get back to you as soon as possible.
-          Alternatively, you can also reach me at francisco@fcosta.pt
-        </p>
-        <form
-          // onSubmit={handleSubmit}
-          action="https://getform.io/f/cca89e21-6ebd-420d-b25a-0f65efbde0aa"
-          method="POST"
-        >
-          <div className={contactStyles.nameForm}>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
-          </div>
-          <div className={contactStyles.emailForm}>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" />
-          </div>
-          <div className={contactStyles.messageForm}>
-            <label htmlFor="message">Message</label>
-            <textarea type="text" id="message" name="message" />
-          </div>
-          <button type="submit">send</button>
-        </form>
-      </div>
+      {messageSent ? (
+        <div className={contactStyles.container}>
+          <p>
+            Thank you for your message! I'll get back to you as soon as
+            possible.
+          </p>
+        </div>
+      ) : (
+        <div className={contactStyles.container}>
+          <p className={contactStyles.message}>
+            Leave me a message and I’ll get back to you as soon as possible.
+            Alternatively, you can also reach me at francisco@fcosta.pt
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className={contactStyles.nameForm}>
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" />
+            </div>
+            <div className={contactStyles.emailForm}>
+              <label htmlFor="email">Email</label>
+              <input type="text" id="email" name="email" />
+            </div>
+            <div className={contactStyles.messageForm}>
+              <label htmlFor="message">Message</label>
+              <textarea type="text" id="message" name="message" />
+            </div>
+            <button type="submit">send</button>
+          </form>
+        </div>
+      )}
     </Layout>
   );
 };
