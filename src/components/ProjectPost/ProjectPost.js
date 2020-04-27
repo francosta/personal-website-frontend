283bd6b98e1702b1ad22a44d7180531fe9266e2c
Francisco Costa <francisco@fcosta.pt>
2020-04-27 19:05:31 +0100

    Fixed navbar margin

--- src/components/ProjectPost/ProjectPost.js
+++ src/components/ProjectPost/ProjectPost.js
@@ -5,11 +5,9 @@ import postStyles from './projectPost.module.scss';
 import globalStyles from '../../styles/styles.module.scss';
 
 // Images import
-import dateIcon from '../../images/date.svg';
-import stackIcon from '../../images/code-icon.svg';
-import summaryIcon from '../../images/summary.svg';
 import Github from '../../images/logos/github.jpg';
 import Youtube from '../../images/logos/youtube.jpg';
+import ProjectDetails from '../ProjectDetails/ProjectDetails';
 
 const Post = ({ post }) => {
   return (
@@ -36,41 +34,7 @@ const Post = ({ post }) => {
           </div>
         </div>
       </section>
-      <section className={postStyles.summaryBox}>
-        <div className={postStyles.specs}>
-          <div className={postStyles.dateBox}>
-            <div className={postStyles.date}>
-              <div className={postStyles.heading}>
-                <span>Date of Project</span>
-              </div>
-              <div className={postStyles.postDate}>
-                <span>{post.publishedDate}</span>
-              </div>
-            </div>
-            <img src={dateIcon} alt="Calendar icon in black and white" />
-          </div>
-          <div className={postStyles.stackBox}>
-            <div className={postStyles.stack}>
-              <div className={postStyles.heading}>Stack</div>
-              <ul>
-                {post.stack.technology.map((technology) => (
-                  <li>{technology.name}</li>
-                ))}
-              </ul>
-            </div>
-            <img src={stackIcon} alt="Programming icon in black and white" />
-          </div>
-        </div>
-        <div className={postStyles.summaryContainer}>
-          <img src={summaryIcon} alt="Text icon in black and white" />
-          <div className={postStyles.summary}>
-            <div className={postStyles.heading}>
-              <span>Summary</span>
-            </div>
-            <p>{post.description}</p>
-          </div>
-        </div>
-      </section>
+      <ProjectDetails post={post} />
       <section>
         <ReactMarkdown source={post.body} />
       </section>
