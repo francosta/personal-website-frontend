8af0a6939f01d91e03dfddd7f11a8b59e56722b8
francosta <francisco@fcosta.pt>
2020-10-15 19:35:43 +0100

    Moved styling to postContainer file.

--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import postContainerStyles from './postContainer.module.scss';
 
 const PostContainer = ({ children }) => {
+  console.log(children);
   return <div className={postContainerStyles.container}>{children}</div>;
 };
 
