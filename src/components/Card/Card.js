8d732963ef964994f86f4115ff1c31c116463ca8
francosta <francisco@fcosta.pt>
2020-10-19 15:57:26 +0100

    Refactored card component.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,76 +6,53 @@ import CardHeading from '../CardHeading/CardHeading';
 import CardDetails from '../CardDetails/CardDetails';
 import CardDescription from '../CardDescription/CardDescription';
 
-const Card = ({ index, post, path }) => {
-  const structure = [1, 2, 3, 4];
-  const ind = index % structure.length;
-  const selector = structure[ind];
-
-  if (selector === 1) {
-    let descriptionExcerpt = post.description;
-    if (post.description.length > 230) {
-      descriptionExcerpt = `${post.description.substring(0, 227)}...`;
-    }
-
-    return (
-      <div className={`${cardStyles.card} ${cardStyles.card1Left}`}>
-        <Link to={`/${post.fields.type}/${post.fields.slug}`}>
-          <CardImage image={post.hero} />
-          <CardHeading path={path} post={post} />
-          <CardDescription descriptionExcerpt={descriptionExcerpt} />
-        </Link>
-        <CardDetails post={post} />
-      </div>
-    );
-  }
-  if (selector === 2) {
-    let descriptionExcerpt = post.description;
-    if (post.description.length > 100) {
-      descriptionExcerpt = `${post.description.substring(0, 97)}...`;
-    }
-
-    return (
-      <div className={`${cardStyles.card} ${cardStyles.card2Right}`}>
-        <Link to={`/${post.fields.type}/${post.fields.slug}`}>
-          <CardImage image={post.hero} />
-          <CardHeading path={path} post={post} />
-          <CardDescription descriptionExcerpt={descriptionExcerpt} />{' '}
-        </Link>
-        <CardDetails post={post} />
-      </div>
-    );
-  }
-  if (selector === 3) {
-    let descriptionExcerpt = post.description;
-    if (post.description.length > 100) {
-      descriptionExcerpt = `${post.description.substring(0, 97)}...`;
-    }
-
-    return (
-      <div className={`${cardStyles.card} ${cardStyles.card2Left}`}>
-        <Link to={`/${post.fields.type}/${post.fields.slug}`}>
-          <CardImage image={post.hero} />
-          <CardHeading path={path} post={post} />
-          <CardDescription descriptionExcerpt={descriptionExcerpt} />{' '}
-        </Link>
-        <CardDetails post={post} />
-      </div>
-    );
-  }
-
-  let descriptionExcerpt = post.description;
-  if (post.description.length > 230) {
-    descriptionExcerpt = `${post.description.substring(0, 227)}...`;
-  }
-
+const CardContent = ({ post, path, descriptionExcerpt }) => {
   return (
-    <div className={`${cardStyles.card} ${cardStyles.card1Right}`}>
+    <>
       <Link to={`/${post.fields.type}/${post.fields.slug}`}>
         <CardImage image={post.hero} />
         <CardHeading path={path} post={post} />
         <CardDescription descriptionExcerpt={descriptionExcerpt} />{' '}
       </Link>
       <CardDetails post={post} />
+    </>
+  );
+};
+
+const Card = ({ index, post, path }) => {
+  const structure = [1, 2, 3, 4];
+  const ind = index % structure.length;
+  const selector = structure[ind];
+
+  const getClass = selector => {
+    switch (selector) {
+      case 1:
+        return cardStyles.card1Left;
+      case 2:
+        return cardStyles.card2Right;
+      case 3:
+        return cardStyles.card2Left;
+      default:
+        return cardStyles.card1Right;
+    }
+  };
+
+  const getDescription = selector => {
+    switch (selector) {
+      case 2 || 3:
+        return `${post.description.substring(0, 97)}...`;
+      default:
+        return `${post.description.substring(0, 125)}...`;
+    }
+  };
+
+  return (
+    <div className={`${cardStyles.card} ${getClass(selector)}`}>
+      <CardContent
+        path={path}
+        descriptionExcerpt={getDescription(selector)}
+        post={post}
+      />
     </div>
   );
 };
