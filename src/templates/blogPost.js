58e3e1b40562f064f93078508e6811869a4b331b
francosta <francisco@fcosta.pt>
2020-10-20 09:39:35 +0100

    Added syntax highlighting

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import { solarizedDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import SEO from '../components/seo';
 import Layout from '../components/Layout/layout';
 import PostContainer from '../components/PostContainer/PostContainer';
 import PostHero from '../components/PostHero/PostHero';
 import BlogPostHeader from '../components/BlogPostHeader/BlogPostHeader';
 
-SyntaxHighlighter.registerLanguage('javascript', js);
-
 const BlogPost = ({ pageContext, path }) => {
   const { post } = pageContext;
-
-  const renderers = {
-    code: ({ value }) => {
-      return (
-        <SyntaxHighlighter
-          style={solarizedDark}
-          language="javascript"
-          children={value}
-        />
-      );
-    },
-  };
 
   return (
     <Layout path={path}>
@@ -35,7 +18,7 @@ const BlogPost = ({ pageContext, path }) => {
           <BlogPostHeader post={post} />
         </section>
         <section id="BlogPost">
-          <ReactMarkdown source={post.body} renderers={renderers} />
+          <ReactMarkdown source={post.body} />
         </section>
       </PostContainer>
     </Layout>
